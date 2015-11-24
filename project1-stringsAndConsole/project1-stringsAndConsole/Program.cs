bb26abef97440bd2fa5e0a460e5cb90e511a58a8
crazySquid1 <clinton.page123@gmail.com>
2015-11-24 16:49:56 +1030

    first commit

---
+++ project1-stringsAndConsole/project1-stringsAndConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1_stringsAndConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter a bunch of numbers. (press CTRL+Z when done)");
+            string line = System.Console.ReadLine();
+            int total = 0;
+            while (line != null)
+            {
+                //shows how to convert between different types
+                UInt32 n = UInt32.Parse(line);
+                total += (int)(n);
+                line = System.Console.ReadLine();
+            }
+            
+            //output an answer
+            System.Console.Write("total={0}\n", total);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ project2-errorhandlingAndObjects/project2-errorhandlingAndObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2_errorhandlingAndObjects
+{
+    class Program
+    {
+        public class BankAccount
+        {
+            public string accountName;
+            private int balance;
+
+            public int GetBalance()
+            {
+                return balance;
+            }
+
+            public void setBalance(int mBalance)
+            {
+                balance = mBalance;
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount bank = new BankAccount();
+            bank.accountName = "Student saver";
+            bank.setBalance(5);
+            System.Console.WriteLine(bank.GetBalance());
+            System.Console.ReadLine();
+
+        }
+    }
+}
