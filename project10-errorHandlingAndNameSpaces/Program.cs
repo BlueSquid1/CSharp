bc92a123bd2e38c8914fb5b28e3208112b65602b
Clinton Page <clinton.page123@gmail.com>
2015-11-27 19:14:05 +1030

    work on namespaces

--- project10-errorHandlingAndNameSpaces/Program.cs
+++ project10-errorHandlingAndNameSpaces/Program.cs
@@ -28,13 +28,19 @@ namespace MyTradingCompany
 	}
 }
 
+using Management = MyTradingCompany.ManagementReporting;
+
 class Program
 {
 	static void Main()
 	{
-		MyTradingCompany.ManagementReporting.SalesReport F = new MyTradingCompany.ManagementReporting.SalesReport();
+		MyTradingCompany.ManagementReporting.SalesReport F1 = new MyTradingCompany.ManagementReporting.SalesReport();
+
+		//which is same as:
+		Management.SalesReport F2 = new Management.SalesReport();
 		
-		Console.WriteLine(F.x.a); //F.x.a = 4
+		Console.WriteLine(F1.x.a); //F1.x.a = 4
+		Console.WriteLine(F2.x.a); //F2.x.a = 4
 		
 		/*
 		string n = null;
