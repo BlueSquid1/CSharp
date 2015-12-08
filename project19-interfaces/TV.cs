a552dcec596d31e9290171bda0639792ee60792b
Clinton <clinton.page123@gmail.com>
2015-12-08 19:28:37 +1030

    interfaces added

---
+++ project19-interfaces/TV.cs
@@ -0,0 +1,52 @@
+using System;
+
+class TV
+{
+	public interface antenna
+	{
+		//all methods/properties are public
+		//cannot declare access modifier (no need)
+		void UpdateChannel(string newChannel);
+		string GetChannel();
+	}
+	
+	//exend the TVRemote interface
+	public interface TVRemote : antenna
+	{
+		void Reset();
+	}
+	
+	internal class Remote : TVRemote
+	{
+		private string curChannel = "none";
+		
+		public void UpdateChannel(string newChannel)
+		{
+			this.curChannel = newChannel;
+		}
+		
+		public string GetChannel()
+		{
+			return this.curChannel;
+		}
+		
+		public void Reset()
+		{
+			this.curChannel = "none";
+		}
+	}
+	
+	public static void Main()
+	{
+		TVRemote myRemote;
+		
+		//simular to:
+		//Remote myRemote;
+		
+		myRemote = new Remote();
+		myRemote.UpdateChannel("ch9");
+		Console.WriteLine(myRemote.GetChannel()); //ch9
+		myRemote.Reset();
+		Console.WriteLine(myRemote.GetChannel()); //none
+	}
+}
